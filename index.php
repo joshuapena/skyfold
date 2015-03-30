a431645be98a2f8f2cc6e5152c8646064907cd40
Joshua Pena <penacalata@gmail.com>
2015-03-29 23:43:39 -0700

    added dynamic content

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 include("templates/header.htm");
-include("templates/index.htm");
+
+$action = 'index';
+$disallowed_paths = array('header', 'footer');
+if (!empty($_GET['action'])) {
+ $tmp_action = basename($_GET['action']);
+ if (!in_array($tmp_action, $disallowed_paths) && file_exists("templates/{$tmp_action}.htm"))
+  $action = $tmp_action;
+}
+include("templates/$action.htm");
+
 include("templates/footer.htm");
 ?>
