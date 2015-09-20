5721f3796dee5154283f9f5585e9d0445e0840ef
Joshua Pena <penacalata@gmail.com>
2015-09-20 01:34:31 -0700

    updated resume

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include("templates/header.htm");
 $action = 'index';
 $disallowed_paths = array('header', 'footer');
 if (!empty($_GET['action'])) {
- $tmp_action = basename($_GET['action']);
- if (!in_array($tmp_action, $disallowed_paths) && file_exists("templates/{$tmp_action}.htm"))
+  $tmp_action = basename($_GET['action']);
+  if (!in_array($tmp_action, $disallowed_paths) && file_exists("templates/{$tmp_action}.htm"))
   $action = $tmp_action;
 }
 include("templates/$action.htm");
